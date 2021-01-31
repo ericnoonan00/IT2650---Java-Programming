9d8ceed50cb3b81725dbe22790c19ac96c280b78
ericnoonan00 <70498751+ericnoonan00@users.noreply.github.com>
2021-01-31 01:13:12 -0500

    Update Main.java

--- code projects/Lab 1 Java - Helicopter Project/src/com/company/Main.java
+++ code projects/Lab 1 Java - Helicopter Project/src/com/company/Main.java
@@ -7,22 +7,29 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         Location[] city = new Location[7];
-        city[0] = new Location(0,0,"Cleveland1", true);
-        city[1] = new Location(0,0,"Cleveland2", true);
-        city[2] = new Location(0,0,"Cleveland3", false);
-        city[3] = new Location(0,0,"Cleveland4", true);
-        city[4] = new Location(0,0,"Cleveland5", true);
-        city[5] = new Location(0,0,"Cleveland6", false);
-        city[6] = new Location(0,0,"Cleveland7", false);
+        city[0] = new Location(0,0,"Cleveland", true);
+        city[1] = new Location(500,-100,"North Pole", true);
+        city[2] = new Location(-50,-50,"Campbell Hill", false);
+        city[3] = new Location(-100,1000,"Tokyo", true);
+        city[4] = new Location(-200,75,"Chicago", true);
+        city[5] = new Location(150,0,"Erie PA", true);
+        city[6] = new Location(300,-75,"Allegheny Nation Forest", false);
 
+        double maxFuel = 0.0;
+        double mpg = 0.0;
+        System.out.print("Enter your desired capacity for your fuel tank: ");
+        maxFuel = input.nextDouble();
+        System.out.print("Enter your desired miles per gallon: ");
+        mpg = input.nextDouble();
 
-        Helicopter heli = new Helicopter(city[0]);
+        Helicopter heli = new Helicopter(maxFuel, mpg, city[0]);
 
-    //GAME LOOP
+        //GAME LOOP
         boolean gameOn = true;
         while(gameOn) {
             heli.showMaxFuel();
             heli.showCurrentFuel();
+            heli.showMpg();
             heli.showCurrentCity();
             System.out.print("""
                     Would u like to do:
@@ -35,16 +42,17 @@ public class Main {
                 System.out.flush();
                 System.out.print("""
                         What city would you like to travel to? (enter a number)\t
-                        \t[1] - Cleveland1
-                        \t[2] - Cleveland2
-                        \t[3] - Cleveland3
-                        \t[4] - Cleveland4
-                        \t[5] - Cleveland5
-                        \t[6] - Cleveland6
-                        \t[7] - Cleveland7
+                        \t[1] - Cleveland
+                        \t[2] - North Pole
+                        \t[3] - Campbell Hill
+                        \t[4] - Tokyo
+                        \t[5] - Chicago
+                        \t[6] - Erie PA
+                        \t[7] - Allegheny Nation Forest
                         --->""");
                 int citySelect = input.nextInt() - 1;
                 heli.travel(city[citySelect]);
+
 
             } else if(userInput == 'r') {
                 heli.refuel();
