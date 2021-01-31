23c431eba0b66836825348880a2ba77d439a30ff
ericnoonan00 <70498751+ericnoonan00@users.noreply.github.com>
2021-01-31 01:13:39 -0500

    Update Location.java

--- code projects/Lab 1 Java - Helicopter Project/src/com/company/Location.java
+++ code projects/Lab 1 Java - Helicopter Project/src/com/company/Location.java
@@ -1,28 +1,34 @@
 package com.company;
 
 public class Location {
-    private String name;
-    private int x;
-    private int y;
-    private boolean hasFuel;
+    private final String name;
+    private final double x;
+    private final double y;
+    private final boolean hasFuel;
 
     public String getName() { return name; }
 
-    public int getX() { return x; }
+    public double getX() { return x; }
 
-    public int getY() { return y; }
+    public double getY() { return y; }
 
     public boolean getHasFuel() { return hasFuel; }
 
-    public Location(int x, int y, String name, boolean hasFuel){
+    public Location(int x, int y, String name, boolean hasFuel) {
         this.x = x;
         this.y = y;
         this.name = name;
         this.hasFuel = hasFuel;
     }
 
-    public String toString(){
-        String x = this.name;
-        return x;
+    public double getDistBetweenCities(Location cCity, Location tCity){
+        double distance;
+        distance = Math.sqrt((Math.pow(tCity.x - cCity.x, 2) + Math.pow(tCity.y - cCity.y, 2)));       //d=√((x_2-x_1)²+(y_2-y_1)²)
+        System.out.println("Distance Traveled: " + distance);
+        return distance;
+    }
+
+    public String toString() {
+        return this.name;
     }
 }
